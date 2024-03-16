d752aa81bdc91c04dd9c9f5b572f95a6c1de3cbb
AliNisar00 <syed.alinisar2002@gmail.com>
2024-03-16 16:01:32 +0500

    Add CustomIcon.ts component to enable custom vector icon usage

---
+++ src/components/CustomIcon.ts
@@ -0,0 +1,5 @@
+// This component allows us to use custom vector icons
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icoMoonConfig from '../../selection.json';
+
+export default createIconSetFromIcoMoon(icoMoonConfig);
