e18ee88ce8a6ed4d7bd76713c42a1a1721d002f8
AliNisar00 <syed.alinisar2002@gmail.com>
2024-03-17 16:20:23 +0500

    Add TabNavigator.tsx with screens set up on tabs

---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import CartScreen from '../screens/CartScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+import CustomIcon from '../components/CustomIcon';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarHideOnKeyboard: true,
+        }}>
+        <Tab.Screen name='Home' component={HomeScreen}></Tab.Screen>
+        <Tab.Screen name='Cart' component={CartScreen}></Tab.Screen>
+        <Tab.Screen name='Favorite' component={FavoritesScreen}></Tab.Screen>
+        <Tab.Screen name='History' component={OrderHistoryScreen}></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TabNavigator;
