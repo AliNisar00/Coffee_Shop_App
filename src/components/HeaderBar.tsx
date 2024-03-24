a3cad3d2321bd50b29303be65a9708154f305b4e
AliNisar00 <syed.alinisar2002@gmail.com>
2024-03-24 18:42:10 +0500

    Add HeaderText with styling in HeaderBar component

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { SPACING } from '../theme/theme';
+import { FONTFAMILY, SPACING, FONTSIZE, COLORS } from '../theme/theme';
 
 interface HeaderBarProps { // Header bar properties
-    title?: string,
+  title?: string, // '?' denotes optional
 }
 
-const HeaderBar: React.FC<HeaderBarProps> = () => {
+const HeaderBar: React.FC<HeaderBarProps> = ({title}) => {
   return (
     <View>
-      <Text>HeaderBar</Text>
+      <Text style={styles.HeaderText}>{title}</Text>
     </View>
   );
 };
@@ -21,6 +21,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
     },
+    HeaderText: {
+      fontFamily: FONTFAMILY.poppins_semibold,
+      fontSize: FONTSIZE.size_20,
+      color: COLORS.primaryWhiteHex,
+    },
 });
 
-export default HeaderBar
+export default HeaderBar;
