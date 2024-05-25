84367ddcdd83b8a341c59f38bd087c3aabda4fa8
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-05-25 16:28:27 +0500

    Add basic item addition to cart

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,18 +6,55 @@ import CoffeeData from "../data/CoffeeData";
 import BeansData from "../data/BeansData";
 
 export const useStore = create(
-    persist(
-        (set, get) => ({
-            CoffeeList: CoffeeData,
-            BeanList: BeansData,
-            CartPrice: 0,
-            FavoritesList: [],
-            CartList: [],
-            OrderHistoryList: [],
-        }),
-        {
-            name: 'coffee-app',
-            storage: createJSONStorage(() => AsyncStorage),
-        },
-    ),
+  persist(
+    (set, get) => ({
+      CoffeeList: CoffeeData,
+      BeanList: BeansData,
+      CartPrice: 0,
+      FavoritesList: [],
+      CartList: [],
+      OrderHistoryList: [],
+      addToCart: (cartItem: any) =>
+        set(
+          produce(state => {
+            let found = false;
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == cartItem.id) {
+                found = true;
+                let size = false;
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (
+                    state.CartList[i].prices[j].size == cartItem.prices[0].size
+                  ) {
+                    size = true;
+                    state.CartList[i].prices[j].quantity++;
+                    break;
+                  }
+                }
+                if (size == false) {
+                  state.CartList[i].prices.push(cartItem.prices[0]);
+                }
+                state.CartList[i].prices.sort((a: any, b: any) => {
+                  if (a.size > b.size) {
+                    return -1;
+                  }
+                  if (a.size < b.size) {
+                    return 1;
+                  }
+                  return 0;
+                });
+                break;
+              }
+            }
+            if (found == false) {
+              state.CartList.push(cartItem);
+            }
+          }),
+        ),
+    }),
+    {
+      name: 'coffee-app',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
 );
